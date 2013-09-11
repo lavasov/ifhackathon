b6b2756fcc926f6acd4a2a4696c55c716ae6168f
Sasha Lavasov <imsashko@gmail.com>
2013-09-11 23:39:47 +0300

    Add files for previous commit

---
+++ javascripts/countdown.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+  // set the date we're counting down to
+  var target_date = new Date("Oct 02, 2013").getTime();
+
+  // variables for time units
+  var days, hours, minutes, seconds;
+
+  // get tag element
+  var countdown = $('#countdown_container');
+  // update the tag with id "countdown" every 1 second
+    console.log(countdown);
+  setInterval(function () {
+
+      // find the amount of "seconds" between now and target
+      var current_date = new Date().getTime();
+      var seconds_left = (target_date - current_date) / 1000;
+
+      // do some time calculations
+      days = parseInt(seconds_left / 86400);
+      seconds_left = seconds_left % 86400;
+
+      hours = parseInt(seconds_left / 3600);
+      seconds_left = seconds_left % 3600;
+
+      minutes = parseInt(seconds_left / 60);
+      seconds = parseInt(seconds_left % 60);
+
+      if (hours < 10) {
+        zeroHours = '0'
+      } else {
+        zeroHours = ''
+      };
+
+      if (minutes < 10) {
+        zeroMinutes = '0'
+      } else {
+        zeroMinutes = ''
+      };
+
+      if (seconds < 10) {
+        zeroSeconds = '0'
+      } else {
+        zeroSeconds = ''
+      };
+
+      // format countdown string + set tag value
+      var text = "<span>" + days + "<img src='images/colon.png'/><i>днів</i></span><span>" + zeroHours + hours + "<img src='images/colon.png'/><i>годин</i></span><span>" + zeroMinutes+minutes + "<img src='images/colon.png'/><i>хвилин</i></span><span>" + zeroSeconds+seconds + "<i>секунд</i></span>";
+      countdown.html(text)
+
+  }, 1000);
+  // $('#countdown_container').html()
+})
