2630afdd9d1403b971318e1c9aa77ced6271c2b5
Sasha Lavasov <imsashko@gmail.com>
2013-09-16 22:48:32 +0300

    Clear js and scss

--- javascripts/smooth_scroll.js
+++ javascripts/smooth_scroll.js
@@ -1,9 +1,9 @@
-$(document).ready(function(){
-  $('.navlink').click(function(e){
+$(document).on('click', '.navlink',
+  function(e){
     e.preventDefault();
     dataslide = $(this).attr('data-slide');
     $('html,body').animate({
       scrollTop: $('section[data-slide="' + dataslide + '"]').offset().top
     }, 1500, 'easeInOutQuint');
-  })
-})
+  }
+)
